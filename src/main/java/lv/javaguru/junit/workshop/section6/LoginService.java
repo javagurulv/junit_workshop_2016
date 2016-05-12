93e2fa654b18c2893bded1370d144bca6e689819
Viktors.Savonins <vik@gmail.com>
2016-05-12 12:36:39 +0300

    refactoring

--- src/main/java/lv/javaguru/junit/workshop/section6/LoginService.java
+++ src/main/java/lv/javaguru/junit/workshop/section6/LoginService.java
@@ -1,42 +1,7 @@
 package lv.javaguru.junit.workshop.section6;
 
-import java.util.Optional;
+public interface LoginService {
 
-public class LoginService {
-
-    private Database database;
-
-
-    public boolean login(String login, String password) {
-
-        if (isLoginNotProvided(login)) {
-            return false;
-        }
-
-        if (isPasswordNotProvided(password)) {
-            return false;
-        }
-
-        Optional<User> userOptional = database.getUserByLogin(login);
-        if (!userOptional.isPresent()) {
-            return false;
-        } else {
-            User userFromDb = userOptional.get();
-            String userFromDbPassword = userFromDb.getPassword();
-            if (!password.equals(userFromDbPassword)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private boolean isLoginNotProvided(String login) {
-        return login == null || login.equals("");
-    }
-
-    private boolean isPasswordNotProvided(String password) {
-        return password == null || password.equals("");
-    }
+    boolean login(String login, String password);
 
 }
--- src/test/java/lv/javaguru/junit/workshop/section6/LoginServiceTest.java
+++ src/test/java/lv/javaguru/junit/workshop/section6/LoginServiceTest.java
@@ -1,69 +1,73 @@
 package lv.javaguru.junit.workshop.section6;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
-@RunWith(MockitoJUnitRunner.class)
 public class LoginServiceTest {
 
-    @Mock
     private Database database;
+    private LoginServiceImpl loginService;
 
-    @InjectMocks
-    private LoginService loginService = new LoginService();
+    private static final String LOGIN = "login";
+    private static final String PASSWORD = "password";
+    private static final String DIFFERENT_PASSWORD = "differentPassword";
 
+
+    @Before
+    public void init() {
+        database = mock(Database.class);
+        loginService = new LoginServiceImpl(database);
+    }
 
     @Test
     public void shouldBePossibilityToProvideLoginAndPassword() {
-        User user = new User("login", "password");
-        doReturn(Optional.of(user)).when(database).getUserByLogin("login");
-        loginService.login("login", "password");
+        User user = new User(LOGIN, PASSWORD);
+        doReturn(Optional.of(user)).when(database).getUserByLogin(LOGIN);
+        loginService.login(LOGIN, PASSWORD);
     }
 
     @Test
     public void whenLoginIsNullThenNotLogin() {
-        boolean loginResult = loginService.login(null, "password");
+        boolean loginResult = loginService.login(null, PASSWORD);
         assertFalse(loginResult);
     }
 
     @Test
     public void whenLoginBlankThenNotLogin() {
-        boolean loginResult = loginService.login("", "password");
+        boolean loginResult = loginService.login("", PASSWORD);
         assertFalse(loginResult);
     }
 
     @Test
     public void whenPasswordIsNullThenNotLogin() {
-        boolean loginResult = loginService.login("login", null);
+        boolean loginResult = loginService.login(LOGIN, null);
         assertFalse(loginResult);
     }
 
     @Test
     public void whenPasswordBlankThenNotLogin() {
-        boolean loginResult = loginService.login("login", "");
+        boolean loginResult = loginService.login(LOGIN, "");
         assertFalse(loginResult);
     }
 
     @Test
     public void whenDatabaseNotContainUserThenNotLogin() {
-        doReturn(Optional.empty()).when(database).getUserByLogin("login");
-        boolean loginResult = loginService.login("login", "password");
+        doReturn(Optional.empty()).when(database).getUserByLogin(LOGIN);
+        boolean loginResult = loginService.login(LOGIN, PASSWORD);
         assertFalse(loginResult);
     }
 
     @Test
     public void whenDatabaseReturnUserWithDifferentPasswordThenNotLogin() {
-        User user = new User("login", "differentPassword");
-        doReturn(Optional.of(user)).when(database).getUserByLogin("login");
-        boolean loginResult = loginService.login("login", "password");
+        User user = new User(LOGIN, DIFFERENT_PASSWORD);
+        doReturn(Optional.of(user)).when(database).getUserByLogin(LOGIN);
+        boolean loginResult = loginService.login(LOGIN, PASSWORD);
         assertFalse(loginResult);
     }
 
